b3535ad231981ac52c8081f0ad0717165a45ed8e
Jacob Greenfield <jacob@jacobgreenfield.me>
2022-01-17 06:55:29 +0000

    tmexclude: add 1.0.0 bottle.

--- Formula/tmexclude.rb
+++ Formula/tmexclude.rb
@@ -6,6 +6,12 @@ class Tmexclude < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/Coder-256/tmexclude.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/Coder-256/homebrew-tmexclude/releases/download/tmexclude-1.0.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur: "8fbc1a2e7085592816f472c12661670b7362a1c86947c299521a618fc9934c85"
+  end
+
   depends_on xcode: ["13", :build]
   depends_on macos: :catalina
 
